9d2921e405e3d35c1983d711692e67b81433ea40
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2021-09-18 11:12:11 -0300

    :sparkles: working with state - hook useState

---
+++ pages/state/mouse.js
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+export default function mouse() {
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+
+    const style = {
+        backgroundColor: 'gray',
+        color: '#fff',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+    }
+
+    function mouseMoving(event) {
+        // errado: precisa alterar através da função
+        // x = event.clientX
+        // y = event.clientY
+        setX(event.clientX)
+        setY(event.clientY)
+    }
+
+    return (
+        <div style={style} onMouseMove={mouseMoving}>
+            <span>Eixo X: {x}</span>
+            <span>Eixo Y: {y}</span>
+        </div>
+    )
+}
